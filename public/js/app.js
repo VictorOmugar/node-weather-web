53f96bf33cfbb2d1afa467f9e981c8292e40319a
Victor Omugar <Vickinsley@SeedboxDeoui-MacBookPro.local>
2022-11-28 13:33:19 +0300

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -6,9 +6,6 @@
 //     })
 // })
 
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 
@@ -25,7 +22,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading....'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location ).then((response) => {
+    fetch('/weather?address=' + location ).then((response) => {
     response.json().then((data) => {
 
         if (data.error) {
